ecf43fd4911be65b8d5f9bd9552709972419ed1b
yosokim <yosokim@outlook.com>
2022-10-29 14:57:02 +0800

    chore: update local patch script

--- script/patch.sh
+++ script/patch.sh
@@ -1,5 +1,9 @@
 git clone --depth=1 git@github.com:fltstore/egao_database.git
 
+# copy with local
+# cp -rf ../../egao_database/* ../assets/database
+
 cp -rf egao_database ../assets/database
 cd ../assets/database
 rm -rf .git
+rm -rf generate.mjs
